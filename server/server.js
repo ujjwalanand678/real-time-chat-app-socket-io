fe26f0022e865ca9fbcda6a52fbd724d6ebcc228
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-10-10 23:55:36 +0530

    connected to database

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
-import { config } from "dotenv"
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
+import { config } from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
-dotenv.config()
-const port = process.env.PORT || 3000 ;
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 3000;
+mongoose.set("strictQuery", false);
 
-app.listen(port , ()=>{ console.log(`server is running at ${port}`)})
+const connectDB = async () => {
+  try {
+    mongoose.connect(process.env.MONGO_URI);
+    console.log(`Connected to MongoDB`);
+  } catch (error) {
+    console.log("error");
+  }
+};
+
+
+connectDB().then(()=>{
+    app.listen(port, ()=>{
+    console.log(`server is running at ${port}`)
+})
+})
